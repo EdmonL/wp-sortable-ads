99ba834ac063cff16ee45a793aa8f195b75ad4ab
Meng Luan <edmonluan@gmail.com>
2018-02-22 19:20:29 -0500

    sanitize and validate site domain

--- sortable-ads/admin/SortableAdsAdmin.php
+++ sortable-ads/admin/SortableAdsAdmin.php
@@ -15,7 +15,10 @@ class SortableAdsAdmin {
         register_setting(
             'srtads',
             'srtads_settings',
-            ['default' => ['site_domain' => home_url()]]
+            [
+                'default' => ['site_domain' => preg_replace('/^http:\\/\\/(www\\.)?/i', '', home_url('', 'http'), 1)],
+                'sanitize_callback' => [$this, 'sanitizeSettings']
+            ]
         );
         add_settings_section('srtads_default_section', null, null, 'srtads_settings_page');
         add_settings_field(
@@ -47,5 +50,16 @@ class SortableAdsAdmin {
 
     public function renderPage($page, $view) {
         require $this->viewsDir . $view . '.php';
+    }
+
+    public function sanitizeSettings($settings) {
+        $output = get_option('srtads_settings');
+        $domain = sanitize_text_field($settings['site_domain']);
+        if (preg_match('/^[a-z](([-a-z0-9])*[a-z0-9])?(\\.[a-z](([-a-z0-9])*[a-z0-9])?)+$/i', $domain)) {
+            $output['site_domain'] = $domain;
+        } else {
+            add_settings_error('srtads_settings', 'invalid_site_domain', __('Please enter a valid site domain.', 'srtads'));
+        }
+        return $output;
     }
 }
--- sortable-ads/sortable-ads.php
+++ sortable-ads/sortable-ads.php
@@ -4,7 +4,7 @@
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           0.0.1
  * Author:            Sortable
- * Author URI:        https://sortable.com/
+ * Author URI:        sortable.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
